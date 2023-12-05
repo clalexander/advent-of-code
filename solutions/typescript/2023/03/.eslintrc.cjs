65f3e716296a7837bf22e0e5c902ea34916b11c4
Chris Alexander <chris@synaptechgenesis.com>
2023-12-05 11:29:36 -0500

    feat: add 2023 03

---
+++ solutions/typescript/2023/03/.eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: '../../../../.eslintrc.cjs',
+  parserOptions: {
+    project: 'solutions/typescript/2023/03/tsconfig.json',
+    sourceType: 'module',
+  },
+}
