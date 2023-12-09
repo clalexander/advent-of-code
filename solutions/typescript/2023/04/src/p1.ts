431fcd468ae5cfea9902ffe13579c23a483659f5
Chris Alexander <clalex005@gmail.com>
2023-12-09 10:12:38 -0500

    feat: add 2023 04

--- solutions/typescript/2023/04/src/p1.ts
+++ solutions/typescript/2023/04/src/p1.ts
@@ -2,55 +2,27 @@ import { task } from '@clalexander/advent-of-code-core';
 import packageJson from '../package.json' assert { type: 'json' };
 
 export const p1 = (input: string): number => {
-  const schematic = input.split('\n');
-
-  let sum = 0;
-  let digit = 0;
-  let curNum = 0;
-  let i;
-  let j;
-  let shouldAdd = false;
-
-  function maybeAddAndReset() {
-    if (shouldAdd) {
-      sum += curNum;
-    }
-    shouldAdd = false;
-    curNum = 0;
-  }
-
-  function check() {
-    if (shouldAdd) {
-      return;
-    }
-
-    for (let ii = i - 1; ii <= i + 1 && ii < schematic.length; ii++) {
-      for (let jj = curNum < 10 ? j - 1 : j + 1; jj <= j + 1 && jj < schematic[0].length; jj++) {
-        if (ii < 0 || jj < 0 || (ii === i && jj === j)) {
-          continue;
+  return input.split('\n')
+    .map((line) => {
+      const [wNums, myNums] = line.split(': ')[1]
+        .split(' | ')
+        .map((nums) => nums.split(' ')
+          .filter(Boolean)
+          .map((num) => parseInt(num, 10))
+          .sort((a, b) => (a - b)));
+      let j = 0;
+      let count = 0;
+      for (let i = 0; i < myNums.length && j < wNums.length; i++) {
+        while (j < wNums.length && myNums[i] > wNums[j]) {
+          j++;
         }
-        if (!/[\d.]/.test(schematic[ii][jj])) {
-          shouldAdd = true;
-          return;
+        if (myNums[i] === wNums[j]) {
+          count++;
         }
       }
-    }
-  }
-
-  for (i = 0; i < schematic.length; i++) {
-    for (j = 0; j < schematic[i].length; j++) {
-      digit = parseInt(schematic[i][j], 10);
-      if (Number.isNaN(digit)) {
-        maybeAddAndReset();
-      } else {
-        curNum = curNum * 10 + digit;
-        check();
-      }
-    }
-    maybeAddAndReset();
-  }
-
-  return sum;
+      return count > 0 ? 2 ** (count - 1) : 0;
+    })
+    .reduce((a, b) => a + b, 0);
 };
 
 await task(p1, packageJson.aoc);
--- solutions/typescript/2023/04/src/p2.ts
+++ solutions/typescript/2023/04/src/p2.ts
@@ -1,66 +1,38 @@
 import { task } from '@clalexander/advent-of-code-core';
 import packageJson from '../package.json' assert { type: 'json' };
 
-type PartNumber = {
-  id: number;
-  part: number;
-};
-
 export const p2 = (input: string): number => {
-  let id = 1;
-  let curPartNumber: PartNumber | null = null;
-  let digit = 0;
-
-  const schematic: (PartNumber | null | true)[][] = input
-    .split('\n')
-    .map((line) => line
-      .split('')
-      .map((char) => {
-        if (char === '.') {
-          curPartNumber = null;
-          return null;
+  const dupsQueue: number[] = [];
+  return input.split('\n')
+    .map((line) => {
+      const [wNums, myNums] = line.split(': ')[1]
+        .split(' | ')
+        .map((nums) => nums.split(' ')
+          .filter(Boolean)
+          .map((num) => parseInt(num, 10))
+          .sort((a, b) => (a - b)));
+      const dups = dupsQueue.shift() || 0;
+      const cards = dups + 1;
+      let j = 0;
+      let count = 0;
+      for (let i = 0; i < myNums.length && j < wNums.length; i++) {
+        while (j < wNums.length && myNums[i] > wNums[j]) {
+          j++;
         }
-        if (char === '*') {
-          curPartNumber = null;
-          return true;
-        }
-        digit = parseInt(char, 10);
-        if (Number.isNaN(digit)) {
-          curPartNumber = null;
-          return null;
-        }
-        if (!curPartNumber) {
-          curPartNumber = { id: id++, part: 0 };
-        }
-        curPartNumber.part = curPartNumber.part * 10 + digit;
-        return curPartNumber;
-      }));
-
-  let sum = 0;
-  let gearParts: PartNumber[] = [];
-
-  for (let i = 0; i < schematic.length; i++) {
-    for (let j = 0; j < schematic[i].length; j++) {
-      if (schematic[i][j] === true) {
-        gearParts = [];
-        for (let ii = i - 1; ii <= i + 1 && ii < schematic.length; ii++) {
-          for (let jj = j - 1; jj <= j + 1 && jj < schematic[0].length; jj++) {
-            if (ii < 0 || jj < 0 || (ii === i && jj === j)) {
-              continue;
-            }
-            if (typeof schematic[ii][jj] === 'object' && schematic[ii][jj] !== null && !gearParts.includes(schematic[ii][jj] as PartNumber)) {
-              gearParts.push(schematic[ii][jj] as PartNumber);
-            }
-          }
-        }
-        if (gearParts.length === 2) {
-          sum += gearParts[0].part * gearParts[1].part;
+        if (myNums[i] === wNums[j]) {
+          count++;
         }
       }
-    }
-  }
-
-  return sum;
+      for (let i = 0; i < count; i++) {
+        if (i < dupsQueue.length) {
+          dupsQueue[i] += cards;
+        } else {
+          dupsQueue.push(cards);
+        }
+      }
+      return cards;
+    })
+    .reduce((a, b) => a + b, 0);
 };
 
 await task(p2, packageJson.aoc);
